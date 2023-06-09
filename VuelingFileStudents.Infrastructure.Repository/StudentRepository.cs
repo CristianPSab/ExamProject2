e3e4f16c1e2f0f319519bfd25f3e85aefa40d917
CristianPSab <crpunti.sab@hotmail.com>
2023-06-10 01:17:55 +0200

    StudentRepository and StudentModel fixed closes #6

---
+++ VuelingFileStudents.Infrastructure.Repository/StudentRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VuelingFileStudents.Transversal.Utilities.Models;
+
+namespace VuelingFileStudents.Infrastructure.Repository
+{
+    public class StudentRepository
+    {
+        public string ExportJson()
+        {
+            return "";
+        }
+
+        public string  ExportTXT(StudentModel studentModel)
+        {
+            var provando = studentModel.ToString();
+
+            string path = "C:\\Users\\cristianps\\Documents\\Ficheros\\ficheroTxt.txt";
+
+            //// Create an instance of StreamReader to read from a file.
+            //// The using statement also closes the StreamReader.
+
+            //using (StreamReader sr = new StreamReader(path))
+            //{
+            //    string line;
+            //    // Read and display lines from the file until the end of
+            //    // the file is reached.
+            //    while ((line = sr.ReadLine()) != null)
+            //    {
+            //        Console.WriteLine(line);
+            //    }
+            //}
+
+            //using (StreamWriter outputF = new StreamWriter(path))
+            //{
+
+            //    outputF.WriteLine(provando);
+
+            //}
+
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(provando);
+            }
+
+
+            return "";
+        }
+
+        public string ExportXML()
+        {
+            return "";
+        }
+    }
+}
---
+++ VuelingFileStudents.Transversal.Utilities/Models/StudentModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VuelingFileStudents.Transversal.Utilities.Models
+{
+    public class StudentModel
+    {
+
+        public int Id { get; set; }
+        public Guid Guid { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+        public DateTimePicker Birthday { get; set; }
+     
+
+        
+
+        public StudentModel(string name, string surname, DateTimePicker birthday)
+        {
+           
+            Guid = Guid.NewGuid();
+            Name = name;
+            Surname = surname;
+            Birthday = birthday;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is StudentModel model &&
+                   Id == model.Id &&
+                   Guid.Equals(model.Guid) &&
+                   Name == model.Name &&
+                   Surname == model.Surname &&
+                   EqualityComparer<DateTimePicker>.Default.Equals(Birthday, model.Birthday);
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 6628628;
+            hashCode = hashCode * -1521134295 + Id.GetHashCode();
+            hashCode = hashCode * -1521134295 + Guid.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Name);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Surname);
+            hashCode = hashCode * -1521134295 + EqualityComparer<DateTimePicker>.Default.GetHashCode(Birthday);
+            return hashCode;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: , {Id}  , " +
+                $"Guid: , {Guid}" +
+                $"Name: , {Name}" +
+                $"Surname: , {Surname}" +
+                $"Birthday: , {Birthday}";
+        }
+    }
+}
